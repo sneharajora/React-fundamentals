2fe4990a12432cc77042a34593a00320f90a5f60
Sneha <sneharajora0110@gmail.com>
2024-12-30 00:14:33 +0530

    add todo

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import ToDo from './componets/ToDo.jsx';
+
+
+const Todo = () => {
+
+     
+  return (
+    <>
+        <ToDo></ToDo>
+    </>
+  )
+}
+
+export default Todo
