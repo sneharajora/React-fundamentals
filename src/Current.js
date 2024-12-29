87ede27e892adc8418b8b2e6f9cdc15af88a0e3e
Sneha <sneharajora0110@gmail.com>
2024-12-29 15:42:53 +0530

    add

---
+++ src/Current.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UseRef from './componets/UseRef.jsx'
+import './index.css';
+
+const Current = () => {
+  return (
+   <>
+    <UseRef></UseRef>
+   </>
+  );
+}
+
+export default Current;
